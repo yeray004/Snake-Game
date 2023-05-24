053916dfc4ed0923d87aa8dc30527162597fcb3f
Yeray Guachetá Carreño <yeye20041@gmail.com>
2023-05-23 22:02:50 -0500

    game over functionality

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-import { SNAKE_SPEED, update as updateSnake, draw as drawSnake } from "./snake.js";
+import { SNAKE_SPEED, update as updateSnake, draw as drawSnake, getSnakeHead, snakeIntersection } from "./snake.js";
 import { update as updateFood, draw as drawFood } from "./food.js";
-
+import { outsideGrid } from "./grid.js";
 
 let lastRenderTime = 0;
-const gameBoard = document.getElementById('game-board')
+let gameOver = false;
+const gameBoard = document.getElementById('game-board');
 
 function main(currentTime){
+
+    if(gameOver){
+        if (confirm('You lost. Press ok to restart.')){
+            location.reload();
+        }
+        return
+    }
+
     window.requestAnimationFrame(main); //Tells the broser when can render the next frame, no matter what
     const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
     if(secondsSinceLastRender < 1 / SNAKE_SPEED) return
@@ -21,10 +30,15 @@ window.requestAnimationFrame(main);
 function update(){
     updateSnake();
     updateFood();
+    checkDeath();
 }
 
 function draw(){
     gameBoard.innerHTML = '' //We remove the previous pieces
     drawSnake(gameBoard);
     drawFood(gameBoard);
+}
+
+function checkDeath(){
+    gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
