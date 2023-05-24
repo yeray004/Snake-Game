7410a3260eee2b4d1105113109a9572222de459a
Yeray Guachetá Carreño <yeye20041@gmail.com>
2023-05-23 19:47:11 -0500

    sarting snake functionality

---
+++ index.js
@@ -0,0 +1,25 @@
+import { SNAKE_SPEED, update as updateSnake, draw as drawSnake } from "./snake.js";
+
+let lastRenderTime = 0;
+const gameBoard = document.getElementById('game-board')
+
+function main(currentTime){
+    window.requestAnimationFrame(main); //Tells the broser when can render the next frame, no matter what
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if(secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+    lastRenderTime = currentTime
+
+    update()
+    draw()
+}
+
+window.requestAnimationFrame(main);
+
+function update(){
+    updateSnake()
+}
+
+function draw(){
+    drawSnake(gameBoard)
+}
